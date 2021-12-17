ae762781aa40fa506f4a4c63328d38e6eb052e65
LukeHarjulin <60152401+LukeHarjulin@users.noreply.github.com>
2021-12-17 11:06:18 +0000

    Init commit

---
+++ src/terraform-training-webapp/terraform-training-webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace terraform_training_webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
